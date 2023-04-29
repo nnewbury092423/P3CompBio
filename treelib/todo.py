08eb1901a18405263d9fdc1cb8c5a6d780408eb4
nnewbury092423 <nnewbury9469@gmail.com>
2023-04-28 23:46:49 -0700

    final

--- treelib/todo.py
+++ treelib/todo.py
@@ -14,41 +14,38 @@ def euler_tour(T): # TODO: COMPLETE THIS FUNCTION
     F = {}
     F[T.root.get_label()] = 0
 
-    visited = []
+    # initialize arrays to keep track
+    #visited = {}
     count = [0]
     layer = 0
-    #eulerar = []
 
-    # TODO: REPLACE WITH YOUR CODE!
-    #print(T)
+
 
     def euler(node,layer):
-        visited.append(node.get_label())
+
+        # add node to visited list (only added once ) and euler tour
+        #visited.append(node.get_label())
         E.append(node.get_label())
+        # count to keep track of when node was visited in euler tour
         count[0] = count[0] + 1
+        # layer is incremented when we look at child nodes
         layer = layer + 1
         for child in node.child_nodes():
-            #import pdb; pdb.set_trace()
-            if child not in visited:
-                    #import pdb; pdb.set_trace()
-                    
-                    if child.get_label() not in F:
-                        F[child.get_label()] = count[0]
-                        H[child.get_label()] = layer
-                    euler(child,layer)
-                    E.append(node.get_label())
-                    count[0] = count[0]+1
+            # check for self referential
+            #if child not in visited:
+            if child.get_label() not in F:
+                F[child.get_label()] = count[0]
+                H[child.get_label()] = layer
+            #add children recusively
+            euler(child,layer)
+            # in each euler call, also add parents after recursively
+            E.append(node.get_label())
+            # increment nodes visited after adding to euler tour
+            count[0] = count[0]+1
 
         
-            #visited.append(node)
-            #other.append(node.get_label())
-            
-        #else:
-         #   import pdb; pdb.set_trace()
-         #   other.append(node.get_parent().get_label())
     euler(T.root,layer)
-    #import pdb; pdb.set_trace()
-    
+
     #raise Exception("NOT IMPLEMENTED ERROR!")
     
     return E,F,H
@@ -59,29 +56,28 @@ def compute_diameter(T): # TODO: COMPLETE THIS FUNCTION
     :param tree: the tree
     :return: a list P containing the diameter path and a floating point number d showing the diameter length
     '''
-    #import pdb; pdb.set_trace()
-
+    #initialize backtracking/
     P = []
     distrec = {}
     paths = {}
     d = 0.0
-    #dists= [0]
    
-    # post order moves 
-    # only one for loop - for each node
+
+# postorder loops through the leaves first
     for node in T.traverse_postorder():
+        
+        #calculate and break at root node
         if node.is_root():
-            #children = node.child_nodes()
             dists =[]
             biurn  = []
+            # in case there are more than 2 children/paths coming into root, we only want to add 2 paths
             for children in node.child_nodes():
                 dists.append(distrec[children] +  children.edge_length)
-                #import pdb; pdb.set_trace()
                 biurn.append(children)
-            #import pdb; pdb.set_trace()
+
+
+            # build path from our paths array, add the two largest distances together
             indices = sorted(range(len(dists)),key=lambda index: dists[index])
-            #sorted(dists) 
-            #import pdb; pdb.set_trace()
             d = dists[indices[-1]] + dists[indices[-2]]
             front = paths[biurn[indices[-1]].get_label()]
             n1 = biurn[indices[-1]].get_label() 
@@ -89,39 +85,30 @@ def compute_diameter(T): # TODO: COMPLETE THIS FUNCTION
             n2 = biurn[indices[-2]].get_label()
             middle = [n1, r, n2]
             back =paths[biurn[indices[-2]].get_label()]
+            
+            # we have to reverse the second path.
             P = front + middle +back[::-1] 
-            #import pdb; pdb.set_trace()
-
             break
 
-            #import pdb; pdb.set_trace()
+        # if the node is a leaf, put in the paths dictionary and the distance recording dictionary as 0. We will reference this node as a child later
         if node.is_leaf():
             distrec[node]  = 0
             paths[node.get_label()] = [] 
         else:
-            # d is nodes
-            # for each d[c] add the edge length
-            # in the node make sorted list o each children
+            # best is the longest distance path so far
             best =0
+            # biurn is recording which child should be added to our path
             biurn  = {}
             for children in node.child_nodes():
-                # node length insident to this node
-                #import pdb; pdb.set_trace()
+                # adding the edge lengths incident on these children, find the one longest distance path so far
                 if best <= distrec[children] +  children.edge_length:
                     best = distrec[children] +  children.edge_length
                     biurn = children
-               # each has longest distance to their children
+            # the longest distance from our node is then recorded in our distrec list 
             distrec[node] = best
-            #import pdb; pdb.set_trace()
+            # this path is also recorded
             paths[biurn.get_label()].append(biurn.get_label())
             paths[node.get_label()] = paths[biurn.get_label()]
-            #dists = sorted([d[c] + c.edge_length for c in node.children])
-            #d[node] = dists[-1]
-            #P = add to path
-        # TODO: REPLACE WITH YOUR CODE!
-        #raise Exception("NOT IMPLEMENTED ERROR!")
-
-
     import pdb; pdb.set_trace()
     return P,d
 
@@ -129,68 +116,64 @@ def find_LCAs(T,Q):
 # find LCA for each of the list of nodes in Q
 # return the label of the LCA node of each query
     
-    #import pdb; pdb.set_trace()
     def __query__(q):
-        #import pdb; pdb.set_trace()  
+
+        # finding the two nodes farthest apart
         indices = []
         for elem in q:
             indices.append(F[elem])
+        
+        
         R = max(indices)
         L = min(indices)
-        #import pdb; pdb.set_trace()
-        i = int(log2(R - L + 1))  
-        #import pdb; pdb.set_trace()
 
+
+        # creating sparse table indices
+        i = int(log2(R - L + 1))  
+        
+        # finding solution preloaded in the sparse table
         option2 = [H[st[L][i]], H[st[R - (2**i) + 1][i]]]
         min_index = min(enumerate(option2), key=lambda x: x[1]) [0]
         if min_index == 0:
             minimum = st[L][i]
         else:
             minimum = st[R - (2**i) + 1][i]
-        #minimum = min(option2)
-        #import pdb; pdb.set_trace()      
+        # returning preloaded soltuion
         return minimum
-
-
-        import pdb; pdb.set_trace()
-        # TODO: REPLACE WITH YOUR CODE!
-        raise Exception("NOT IMPLEMENTED ERROR")
-        #return None
     E,F,H = euler_tour(T) 
 
 
-    # generate the sparse table 
-    # allocate space
+    # allocate space for the sparse table
     MAX = len(E) + 1
     st = [[0 for i in range(MAX)]
-                 for j in range(MAX)]
-
-   # import pdb; pdb.set_trace()   
+                 for j in range(MAX)]   
     
+    # The first entries range of 1
     for i in range(0, len(E)):
         st[i][0] = E[i]
 
-    #import pdb; pdb.set_trace() 
+
+    # preload solutions into sparse table
     j = 1
     while 2**j <= len(E):
-        i = 0       #(int i = 1; i <= K; i++)
-        while i + 2**j -1 < len(E):   # (int j = 0; j + (1 << i) <= N; j++)    
+        i = 0       
+        while i + 2**j -1 < len(E):
+
+            # find the minimum of our ranges dynamically, using previous range minimum solutions       
             option =[H[st[i][j-1]], H[st[i + 2**(j-1)][j-1]]]            
             min_index = min(enumerate(option), key=lambda x: x[1]) [0]
             if min_index == 0:
                 st[i][j] = st[i][j-1]
             else:
                 st[i][j] = st[i + 2**(j-1)][j-1]
-             
             i = i+ 1
-            #querey here thingy
         j = j+1
-    #import pdb; pdb.set_trace()   
+    
+
+    # initialize LCA array
     LCAs = []
-    #test = Q[10]
-    #lca = __query__(test)
+    # call our query function  after preloading our solutions
     for q in Q:
         lca = __query__(q)
         LCAs.append(lca)
-    #import pdb; pdb.set_trace()
     return LCAs    
